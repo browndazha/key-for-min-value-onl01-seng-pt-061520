2c8e592cd92d0d379011c6f150ce535428250675
Dazha Brown <dajahbrown@hotmail.com>
2020-06-29 05:31:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,10 @@
 def key_for_min_value(name_hash)
     low_value = nil
     low_key = nil
-    
+
     name_hash.each do |key, value|
 
-      
+
       if low_value == nil
         low_value = value
         low_key = key
